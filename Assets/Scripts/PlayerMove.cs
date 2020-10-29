85df64da485de00578a845388f097e2f0050d3d9
unknown <cloudbridge@kolon.com>
2020-10-29 14:56:32 +0900

    'B16'

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {  
     public float maxSpeed;
+    public float jumpPower;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator animator;
@@ -16,6 +17,13 @@ public class PlayerMove : MonoBehaviour
     }
 
     private void Update() {
+        // Jump
+        if (Input.GetButtonDown("Jump")&& !animator.GetBool("isJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            animator.SetBool("isJumping", true);
+        }
+
         // Stop Speed
         if (Input.GetButtonUp("Horizontal"))
         {
@@ -54,6 +62,25 @@ public class PlayerMove : MonoBehaviour
         else if (rigid.velocity.x < maxSpeed * -1)
         {
             rigid.velocity = new Vector2(maxSpeed * -1, rigid.velocity.y);
+        }
+
+        // RayCast : 오브젝트 검색을 위해 Ray를 쏘는 방식
+        // Landing Platform
+        if (rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down ,new Color(0, 1, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance <  0.5f)
+                {
+                    // Debug.Log(rayHit.collider.name);
+                    animator.SetBool("isJumping", false);
+
+                }
+            
+            }            
         }
     }
 }
